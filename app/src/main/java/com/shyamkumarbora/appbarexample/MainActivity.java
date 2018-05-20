bfdd67abe0a91f7516d0a3a7ade1515a8509efa9
Shyam Kumar Bora <shyamkumarbora17@gmail.com>
2018-05-20 10:46:17 +0530

    Handled menu item click event

--- app/src/main/java/com/shyamkumarbora/appbarexample/MainActivity.java
+++ app/src/main/java/com/shyamkumarbora/appbarexample/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar mToolbar;
@@ -21,5 +23,19 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.app_bar_menu,menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_share:
+                Toast.makeText(this,"Share option is selected",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_settings:
+                Toast.makeText(this,"Settings option is selected",Toast.LENGTH_SHORT).show();
+                return true;
+            default:return super.onOptionsItemSelected(item);
+        }
+
     }
 }
