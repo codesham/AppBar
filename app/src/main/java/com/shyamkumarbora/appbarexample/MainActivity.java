6e16109a4e5ea0895e1d890cc736626a2e5e2575
Shyam Kumar Bora <shyamkumarbora17@gmail.com>
2018-05-20 10:35:02 +0530

    Menu Inflated

--- app/src/main/java/com/shyamkumarbora/appbarexample/MainActivity.java
+++ app/src/main/java/com/shyamkumarbora/appbarexample/MainActivity.java
@@ -3,6 +3,8 @@ package com.shyamkumarbora.appbarexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar mToolbar;
@@ -12,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mToolbar = findViewById(R.id.toolBar);
         setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.app_bar_menu,menu);
+        return true;
     }
 }
